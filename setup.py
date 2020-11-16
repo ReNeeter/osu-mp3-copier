096a1106a4a705cc61b7a84b1ff870b636448915
ReNeeter <69535871+ReNeeter@users.noreply.github.com>
2020-11-16 15:27:24 +0900

    ビルドスクリプトを整理

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@ import sys
 from cx_Freeze import setup, Executable
 
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-
 setup(
     name="osu! Music Copier",
     version="1.2",
     description="osu!の譜面フォルダーから音楽ファイルをコピーします。",
-    executables=[Executable("osu-Music-Copier.py", base=base)],
+    executables=[Executable("osu-Music-Copier.py")],
 )
